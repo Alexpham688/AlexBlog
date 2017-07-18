b1cc92ffe81fc35c9323800fc88f71aa29bc035c
Alexpham688 <Alexpham688@gmail.com>
2017-07-18 15:26:52 -0400

    added slider

--- app.js
+++ app.js
@@ -1,4 +1,49 @@
-(function() {
+$(document).ready(function(){
+
+  var speed = 500;
+  var autoSwitch = true;
+  var autoSwitchSpeed= 4000;
+
+  $(".slide").first().addClass("active");
+
+  //hide all slides
+  $(".slide").hide();
+  // show first slide
+  $(".active").show();
+     //next handler
+  $("#next").on("click",nextSlide);
+
+   // prev handler
+  $("#prev").on("click", prevSlide);
+   //auto slider handler
+  if(autoSwitch === true) {
+    setInterval(nextSlide,autoSwitchSpeed);
+  }
+
+  // switch to next slide
+  function nextSlide(){
+    $(".active").removeClass("active").addClass("lastActive");
+    if($(".lastActive").is(":last-child")){
+      $(".slide").first().addClass("active");
+    } else {
+      $(".lastActive").next().addClass("active");
+    }
+    $(".lastActive").removeClass("lastActive");
+    $(".slide").fadeOut(speed);
+    $(".active").fadeIn(speed);
+  };
+  function prevSlide() {
+    $(".active").removeClass("active").addClass("lastActive");
+    if($(".lastActive").is(":first-child")){
+      $(".slide").last().addClass("active");
+    } else {
+      $(".lastActive").prev().addClass("active");
+    }
+    $(".lastActive").removeClass("lastActive");
+    $(".slide").fadeOut(speed);
+    $(".active").fadeIn(speed);
+    };
+
   var co = $('section').find("span.co").each(function() {
     var $this = $(this);
     $("<blockquote></blockquote>", {
@@ -6,6 +51,6 @@
       text: $this.text()
     }).prependTo( $this.closest('p') );
   });
-   
 
-})();
+
+});
